993e9c67e298d7ad653eb5222831f2d9b19a2dfc
Lustre <firelustre@163.com>
2018-11-15 10:27:08 +0800

    db adapter

--- src/Light/Database/DBAdapter.php
+++ src/Light/Database/DBAdapter.php
@@ -10,6 +10,18 @@ namespace Light\Database;
 
 interface DBAdapter
 {
+    /**
+     * 数据库连接
+     * 数据库如果功能做的单一，如只有query
+     * 而如果又要做ORM，需要花大量的时间，所以还是打算把model层的实现放在最后
+     *
+     * @param $host
+     * @param $root
+     * @param $password
+     * @param $dbname
+     * @param $port
+     * @return mixed
+     */
     public function connection($host, $root, $password, $dbname, $port);
 
     public function query();
